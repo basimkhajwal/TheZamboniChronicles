ac11aff427de2e9871cd77574aad9db81516700d
Basim Khajwal <basimkhajwal@gmail.com>
2015-01-24 22:36:25 +0000

    Added an fpslogger and its implementation

---
+++ js/engine/fpsLogger.js
@@ -0,0 +1,27 @@
+Game.FPSLogger = {
+
+    create: function () {
+
+        var currentDelta = 0;
+        var frames = 0;
+
+        //Return a closure
+        return {
+
+            log: function (delta) {
+                currentDelta += delta;
+                frames++;
+
+                if (currentDelta > 1){
+                    console.log("FPS Logger: " + frames + "fps");
+
+                    currentDelta = 0;
+                    frames = 0;
+                }
+            }
+
+        };
+
+    }
+
+};
--- js/engine/game.js
+++ js/engine/game.js
@@ -1,39 +1,39 @@
-Game.Game = function () {
+Game.Game = {
 
-    this.canvas = null;
-    this.timer = null;
+    create: function () {
 
-};
+        //Create the 'private' variables, needed in the closure
+        var canvas = Game.Canvas.create(1000, 600);
+        var fpsLogger = Game.FPSLogger.create();
+        var timer = null;
 
-Game.Game.prototype = {
-    Initialise: function () {
-        this.canvas = Game.Canvas.create(1000, 600);
-    },
+        //Return the closure
+        return {
 
+            start: function () {
 
-    Start: function () {
-        var that = this;
+                var update = function (delta) {
+                    fpsLogger.log(delta);
+                };
 
-        var update = function (delta) {
+                var render =  function () {
+                    canvas.begin();
+
+                    canvas.getContext().fillRect(10,10,20,20);
+
+                    canvas.end();
+                };
+
+                timer = Game.Timer.create(update, render);
+
+                //Begin the game loop
+                timer.start();
+            }
 
         };
 
-        var render =  function () {
-            that.canvas.begin();
-
-            that.canvas.getContext().fillRect(10,10,20,20);
-
-            that.canvas.end();
-        };
-
-        var timer = Game.Timer.create(update, render);
-
-        //Begin the game loop
-        timer.start();
-    },
+    }
 
 
-
-
-};
+}
 
