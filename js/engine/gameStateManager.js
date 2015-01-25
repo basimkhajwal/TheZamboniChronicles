c3cd9a52fb62b8cb6327a128537e2954fbd30bde
Basim Khajwal <basimkhajwal@gmail.com>
2015-01-25 14:49:21 +0000

    Made base code for the game state manager

--- js/engine/gameStateManager.js
+++ js/engine/gameStateManager.js
@@ -9,8 +9,35 @@ Game.GameStateManager = {
         //The closure
         return {
 
+            pushActivity: function (activity) {
 
+                if( currentActivity !== null){
+                    activityStack.push(currentActivity);
+                    currentActivity.onPause();
+                }
 
+                currentActivity = activity;
+                currentActivity.onCreate();
+            },
+
+            popActivity: function () {
+
+                currentActivity.onDestroy();
+                var oldActivity = currentActivity;
+
+                currentActivity = activityStack.pop();
+                currentActivity.onResume();
+
+                return oldActivity;
+            },
+
+            update: function (delta) {
+                currentActivity.update(delta);
+            },
+
+            render: function (context) {
+                currentActivity.render(context);
+            }
 
         };
 
