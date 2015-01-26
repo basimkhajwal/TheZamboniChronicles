223d5c43117999d8177862ebf85163ff2fd08609
Basim Khajwal <basimkhajwal@gmail.com>
2015-01-26 21:21:25 +0000

    Started writing assetmanager

---
+++ js/engine/assetManager.js
@@ -0,0 +1,101 @@
+Engine.AssetManager = {
+
+    create: function () {
+
+        //The closure
+        var that = {};
+
+        //Variables
+        var downloadQueue = [];
+        var soundsQueue = [];
+        var cache = {};
+        var successCount = 0;
+        var errorCount = 0;
+
+        //Add the methods
+        that.prototype = {
+
+            queueDownlaod: function (path) {
+                downloadQueue.push(path);
+            },
+
+            queueSound: function (id, path) {
+                soundsQueue.push({id: id, path: path})
+            },
+
+            downloadAll = function(downloadCallback) {
+                if (downloadQueue.length === 0 && soundsQueue.length === 0) {
+                    downloadCallback();
+                }
+
+                that.downloadSounds(downloadCallback);
+
+                for (var i = 0; i < this.downloadQueue.length; i++) {
+
+                    var path = this.downloadQueue[i];
+                    var img = new Image();
+                    var that = this;
+
+                    img.addEventListener("load", function() {
+                        console.log(this.src + ' is loaded');
+                        that.successCount += 1;
+
+                        if (that.isDone()) {
+                            downloadCallback();
+                        }
+                    }, false);
+
+                    img.addEventListener("error", function() {
+                        that.errorCount += 1;
+
+                        if (that.isDone()) {
+                            downloadCallback();
+                        }
+                    }, false);
+
+                    img.src = path;
+                    this.cache[path] = img;
+                }
+
+
+            },
+
+            downloadSound: function(id, path, soundsCallback) {
+                cache[path] = soundManager.createSound({
+                    id: id,
+                    autoLoad: true,
+                    url: path,
+                    onload: function() {
+                        console.log(this.url + ' is loaded');
+                        that.successCount += 1;
+
+                        if (that.isDone()) {
+                            soundsCallback();
+                        }
+                    }
+                });
+            },
+
+            getSound: function (path) {
+                return cache[path];
+            },
+
+            getAsset: function (path) {
+                return cache[path];
+            },
+
+
+            isDone: function () {
+                return (downloadQueue.length + soundsQueue.length) === (successCount + errorCount);
+            }
+
+
+
+        };
+
+        //
+
+    }
+
+
+};
--- js/engine/game.js
+++ js/engine/game.js
@@ -1,11 +1,11 @@
-Game.Game = {
+Engine.Game = {
 
     create: function () {
 
         //Create the 'private' variables, needed in the closure
         var canvas = Engine.Canvas.create(1000, 600);
         var fpsLogger = Engine.FPSLogger.create();
-        var gameStateManager = Engine.GameStateManager
+        var gameStateManager = Engine.GameStateManager.create();
         var timer = null;
 
         //Return the closure
