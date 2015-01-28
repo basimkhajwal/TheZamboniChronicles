1351e2a04b9c5867a15cdbd8cfccc31222612dd3
Basim Khajwal <basimkhajwal@gmail.com>
2015-01-28 22:54:03 +0000

    Tidied up code, jslint style

--- js/engine/timer.js
+++ js/engine/timer.js
@@ -5,25 +5,28 @@ var Engine = Engine || {};
 Engine.Timer = {
 
     create: function () {
+        "use strict";
+
+        //Try and get the animation frame from the window, any possible way, otherwise default to the inefficient timer interval method with a callback
+        var requestAnimFrame = (function () {
+
+            return window.requestAnimationFrame        ||
+                window.webkitRequestAnimationFrame     ||
+                window.mozRequestAnimationFrame        ||
+                window.oRequestAnimationFrame          ||
+                window.msRequestAnimationFrame         ||
+
+                function (callback) {
+                    window.setTimeout(callback, 1000 / 60);
+                };
+        }());
+
+        var lastTime = 0;
 
         //Return a closure
         return {
             start: function (updateCallBack, renderCallBack) {
 
-                //Try and get the animation frame from the window, any possible way, otherwise default to the inefficient timer interval method with a callback
-                var requestAnimFrame = (function () {
-                    return window.requestAnimationFrame        ||
-                        window.webkitRequestAnimationFrame     ||
-                        window.mozRequestAnimationFrame        ||
-                        window.oRequestAnimationFrame          ||
-                        window.msRequestAnimationFrame         ||
-
-                        function (callback) {
-                            window.setTimeout(callback, 1000 / 60);
-                        };
-                })();
-
-                var lastTime = 0;
 
                 function main() {
                     var now = Date.now();
