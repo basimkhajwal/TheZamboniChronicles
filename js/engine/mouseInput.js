3f46c99358469c8c13407a7d5622b8222c4797ee
Basim Khajwal <basimkhajwal@gmail.com>
2015-01-28 18:11:06 +0000

    Created basis for mouse input

---
+++ js/engine/mouseInput.js
@@ -0,0 +1,38 @@
+//Implements an abstracted set of methods of getting information about the mouse
+Engine.MouseInput = function () {
+
+    //Get the canvas element and initialise some variables
+    var canvas = document.getElementById("canvas");
+    var mousePos = {
+        x:0,
+        y:0,
+    };
+    var mouseDown = false;
+
+    //Utility function to calculate the relative position from the absolute window position
+    var calcRelativePosition = function (evt) {
+
+        //Get the bounding rectangle of the canvas
+        var rect = canvas.getBoundingClientRect();
+
+        //Update the mousePos as the absolute minus the current position of the canvas
+        mousePos.x = evt.clientX - rect.left;
+        mousePos.y = evt.clientY - rect.top;
+    };
+
+
+    return {
+
+        getMousePos: function () {
+
+
+
+        },
+
+        isMouseDown: function () {
+
+        }
+
+    };
+
+}();
