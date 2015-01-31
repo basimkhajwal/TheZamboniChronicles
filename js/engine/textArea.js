680edaa37d0dd9e627b9d51a850be105f7007c2f
Basim Khajwal <basimkhajwal@gmail.com>
2015-01-31 20:57:41 +0000

    Base getter and setters and methods for text class

--- js/engine/textArea.js
+++ js/engine/textArea.js
@@ -2,17 +2,70 @@ var Engine = Engine || {};
 
 Engine.TextArea = {
 
-    create: function (x, y, text, colour, align) {
+    create: function (x, y, text, colour, family, align, variant) {
         "use strict";
 
         colour = colour || "#000";
+        family = family || "arial";
         align = align || "center";
-
+        variant = variant || "normal";
 
         return {
 
 
+            render: function (context) {
 
+
+
+            },
+
+            setVariant: function (newVariant) {
+                variant = newVariant;
+            },
+
+            getVariant: function () {
+                return variant;
+            },
+
+            setFamily: function (newFamily) {
+                family = newFamily;
+            },
+
+            getFamily: function () {
+                return family;
+            },
+
+            setAlign: function (newAlign) {
+                align = newAlign;
+            },
+
+            getAlign: function () {
+                return align;
+            },
+
+            setColour: function (newColour) {
+                colour = newColour;
+            },
+
+            getColour: function () {
+                return colour;
+            },
+
+            setX: function (newX) {
+                x = newX;
+            },
+
+            getX: function () {
+                return x;
+            },
+
+            setY: function (newY) {
+                y = newY;
+            },
+
+            getY: function () {
+                return y;
+            }
 
         };
 
