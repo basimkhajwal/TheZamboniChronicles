856b8572a9d3e8dd3249b07ff5dd5b4c2a18b9c5
Basim Khajwal <basimkhajwal@gmail.com>
2015-02-02 18:23:02 +0000

    Added new assets file and begun writing it

---
+++ js/utils/assets.js
@@ -0,0 +1,15 @@
+//The module(s)
+var Zamboni = Zamboni || {};
+
+//Sub module(s)
+Zamboni.Utils = Zamboni.Utils || {};
+
+
+
+Zamboni.Utils.Assets = {
+
+    images: [
+        "img/test.png"
+    ]
+
+};
