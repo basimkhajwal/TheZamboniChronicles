772e61208838e0b5bbe1f0235870e5c34fa67f6d
Basim Khajwal <basimkhajwal@gmail.com>
2015-02-03 20:30:40 +0000

    Finished loading bar and added it to the scripts

--- js/engine/ui/loadingBar.js
+++ js/engine/ui/loadingBar.js
@@ -6,9 +6,29 @@ Engine.UI = Engine.UI || {};
 Engine.UI.LoadingBar = {
 
 
-    create: function () {
+    create: function (barY, barWidth, barHeight, canvasWidth, barColour) {
+        "use strict";
 
+        var currentPercentage = 0;
+        var barX = (canvasWidth - barWidth) / 2;
 
-    };
+        return {
+
+            render: function (ctx) {
+                ctx.fillStyle = barColour;
+                ctx.fillRect(barX, barY, barWidth * currentPercentage, barHeight);
+            },
+
+            setPercentage: function (p) {
+                currentPercentage = p;
+            },
+
+            getPercentage: function () {
+                return currentPercentage;
+            }
+
+        };
+
+    }
 
 };
