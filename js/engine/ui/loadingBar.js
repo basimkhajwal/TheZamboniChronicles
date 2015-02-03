d186e0cd6d016ab06c64e6169832585fe1efb326
Basim Khajwal <basimkhajwal@gmail.com>
2015-02-03 21:00:19 +0000

    Finished loading state and added lots of getters and setters

--- js/engine/ui/loadingBar.js
+++ js/engine/ui/loadingBar.js
@@ -9,6 +9,12 @@ Engine.UI.LoadingBar = {
     create: function (barY, barWidth, barHeight, canvasWidth, barColour) {
         "use strict";
 
+        var barColour = barColour || "#000";
+        var canvasWidth = canvasWidth || 1000;
+        var barHeight = barHeight || 30;
+        var barWidth = barWidth || canvasWidth;
+        var barY = barY || 100;
+
         var currentPercentage = 0;
         var barX = (canvasWidth - barWidth) / 2;
 
@@ -19,12 +25,62 @@ Engine.UI.LoadingBar = {
                 ctx.fillRect(barX, barY, barWidth * currentPercentage, barHeight);
             },
 
+            //The getters and setters
+
             setPercentage: function (p) {
                 currentPercentage = p;
             },
 
             getPercentage: function () {
                 return currentPercentage;
+            },
+
+            setY: function (y) {
+                barY = y;
+            },
+
+            getY: function () {
+                return barY;
+            },
+
+            setX: function (x) {
+                barX = x;
+            },
+
+            getX: function () {
+                return barX;
+            },
+
+            setWidth: function (width) {
+                barWidth = width;
+            },
+
+            getWidth: function () {
+                return barWidth;
+            },
+
+            setHeight: function (height) {
+                barHeight = height;
+            },
+
+            getHeight: function () {
+                return barHeight;
+            },
+
+            setCanvasWidth: function (width) {
+                canvasWidth = width;
+            },
+
+            getCanvasWidth: function () {
+                return canvasWidth;
+            },
+
+            setColour: function (colour) {
+                barColour = colour;
+            },
+
+            getColour: function () {
+                return barColour;
             }
 
         };
--- js/states/loadingState.js
+++ js/states/loadingState.js
@@ -14,28 +14,56 @@ Zamboni.States.LoadingState = {
 
         var game = null;
         var state = Engine.GameState.create();
+        var startedChanging = false;
+        var changing = false;
 
         var loadingText = Engine.UI.TextArea.create(Zamboni.Utils.GameSettings.canvasWidth / 2, 300, "Loading..");
         loadingText.setSize(30);
-        loadingText.set
         loadingText.setFamily(Zamboni.Utils.GameSettings.gameFont);
         loadingText.setColour(Zamboni.Utils.ColourScheme.SUN_FLOWER);
 
+        var loadingBar = Engine.UI.LoadingBar.create(320, 800, 30, 1000);
+        loadingBar.setColour(Zamboni.Utils.ColourScheme.POMEGRANATE);
+
         state.onCreate = function (g) {
             game = g;
+            var i;
 
+            for (i = 0; i < Zamboni.Utils.Assets.images.length; i += 1) {
+                Engine.AssetManager.queueDownload(Zamboni.Utils.Assets.images[i]);
+            }
 
+            Engine.AssetManager.downloadAll();
         };
 
         state.update = function (delta) {
+            loadingBar.setPercentage(Engine.AssetManager.getProgress());
 
+            if (Engine.AssetManager.isDone() && !startedChanging) {
+                startedChanging = true;
+
+                window.setInterval(function () {
+                    changing = true;
+                }, 500);
+            }
+
+            if (changing) {
+                var gsm = game.getGameStateManager();
+
+                gsm.setState();
+            }
         };
 
         state.render = function (ctx) {
+            //Custom background colour
             ctx.fillStyle = Zamboni.Utils.ColourScheme.ALIZARIN;
             ctx.fillRect(0, 0, 1000, 600);
 
+            //Draw the text
             loadingText.render(ctx);
+
+            //Draw the loading bar
+            loadingBar.render(ctx);
         };
 
         return state;
