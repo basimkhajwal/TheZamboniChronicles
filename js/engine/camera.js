514877dfc6d22a0226158b2c8f454a7913b4bc77
Basim Khajwal <basimkhajwal@gmail.com>
2015-02-06 20:05:59 +0000

    Made a camera implementation

---
+++ js/engine/camera.js
@@ -0,0 +1,98 @@
+var Engine = Engine || {};
+
+Engine.Camera = {
+
+    create: function (x, y, angle) {
+        "use strict";
+
+        x = (typeof x === "undefined") ? 0 : x;
+        y = (typeof x === "undefined") ? 0 : y;
+        angle = (typeof angle === "undefined") ? 0 : angle;
+
+        var toDegrees = function (rad) {
+            return rad * (180 / Math.PI);
+        };
+
+        var toRadians = function (deg) {
+            return deg * (Math.PI / 180);
+        };
+
+        var angleInRad = toRadians(angle);
+
+        return {
+
+            projectContext: function (ctx) {
+                ctx.translate(x, y);
+                ctx.rotate(angleInRad);
+            },
+
+            unProjectContext: function (ctx) {
+                ctx.translate(-x, -y);
+                ctx.rotate(-angleInRad);
+            },
+
+            projectPoint: function (pX, pY) {
+                pX -= x;
+                pY -= y;
+
+                pX = pX * Math.cos(angleInRad) - y * Math.sin(angleInRad);
+                pY = pX * Math.sin(angleInRad) + y * Math.cos(angleInRad);
+
+                return {
+                    x: pX,
+                    y: pY
+                };
+            },
+
+            unProjectPoint: function (pX, pY) {
+                pX = pX * Math.cos(-angleInRad) - y * Math.sin(-angleInRad);
+                pY = pX * Math.sin(-angleInRad) + y * Math.cos(-angleInRad);
+
+                pX += x;
+                pY += y;
+
+                return {
+                    x: pX,
+                    y: pY
+                };
+            },
+
+            translate: function (vx, vy) {
+                x += vx;
+                y += vy;
+            },
+
+            rotate: function (amount) {
+                angle += amount;
+                angleInRad = toRadians(angle);
+            },
+
+            setX: function (newX) {
+                x = newX;
+            },
+
+            getX: function () {
+                return x;
+            },
+
+            setY: function (newY) {
+                y = newY;
+            },
+
+            getY: function () {
+                return y;
+            },
+
+            setAngle: function (newAngle) {
+                angle = newAngle;
+                angleInRad = toRadians(angle);
+            },
+
+            getAngle: function () {
+                return angle;
+            }
+
+        };
+    }
+
+};
