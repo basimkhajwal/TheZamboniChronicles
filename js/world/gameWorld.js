0749f9f61026eb65eca884d9401d0e097486a78b
Basim Khajwal <basimkhajwal@gmail.com>
2015-02-07 16:33:12 +0000

    Made base stuff for world and commented it

--- js/world/gameWorld.js
+++ js/world/gameWorld.js
@@ -7,3 +7,35 @@ Engine.UI = Engine.UI || {};
 
 Zamboni.States = Zamboni.States || {};
 Zamboni.Utils = Zamboni.Utils || {};
+Zamboni.World = Zamboni.World || {};
+
+/*
+*   The main world class that will contain the current level and any game object (players, enemies etc.)
+*/
+Zamboni.World.GameWorld = {
+
+    /*
+    *   Create a new empty world
+    */
+    create: function () {
+
+        //All the private methods and variables
+
+        //Return all the public methods and variables
+        return {
+
+            //Render the world on the context ctx
+            render: function (ctx) {
+
+            },
+
+            //Update the world with time delta
+            update: function (delta) {
+
+            }
+
+
+        };
+    }
+
+};
