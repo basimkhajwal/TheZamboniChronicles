9b963373dd1abe6f134eba54358c6b457900a5ca
Basim Khajwal <basimkhajwal@gmail.com>
2015-02-08 10:48:35 +0000

    Added some test code

--- js/world/levelParser.js
+++ js/world/levelParser.js
@@ -13,9 +13,31 @@ Zamboni.World = Zamboni.World || {};
 Zamboni.World.LevelParser = (function () {
     "use strict";
 
-    this.parse = function (fileText) {
-        var jsonObj = JSON.parse(fileText);
+    var get = function (url, onsuccess) {
+        var request = new XMLHttpRequest();
+
+        request.onreadystatechange = function () {
+
+            if ((request.readyState === 4) && (request.status === 200)) {
+                onsuccess(request);
+            }
+        };
+
+        request.open("GET", url, true);
+        request.send();
     };
 
 
+    var self = {};
+
+    self.parse = function (file) {
+
+        get(file, function (req) {
+            console.log(req.responseText);
+        });
+
+
+    };
+
+    return self;
 }());
