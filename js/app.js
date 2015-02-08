4f08931c5bf31c1878cc2dce90c131bdc942df14
Basim Khajwal <basimkhajwal@gmail.com>
2015-02-08 18:46:48 +0000

    Removed test code and un-needed files

--- js/app.js
+++ js/app.js
@@ -13,40 +13,6 @@ Zamboni.Utils = Zamboni.Utils || {};
 
 Engine.UI = Engine.UI || {};
 
-var testState = (function () {
-    "use strict";
-
-    var state = Engine.GameState.create();
-
-    var currentX = 10;
-    var currentY = 100;
-    var myText = Engine.UI.TextArea.create(200, 200, "SOME RANDOM TEST", 20, "#FF0000", "cursive", "bold");
-    var velocity = 100;
-
-    state.update = function (delta) {
-        currentX += velocity * delta;
-        currentY += velocity * delta;
-
-        if (currentX > 300 || currentX < 0) {
-            currentX = 150;
-            velocity *= -1;
-            currentY = 250;
-        }
-
-    };
-
-    state.render = function (ctx) {
-        ctx.fillRect(50, 50, 100, 100);
-        ctx.fillRect(currentX, currentY, 10, 10);
-        myText.render(ctx);
-    };
-
-
-    return state;
-
-}());
-
-
 
 var startGame = function () {
     "use strict";
