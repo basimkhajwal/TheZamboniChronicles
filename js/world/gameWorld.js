9287de763d2e138a4319cac87013657b13bac0a3
Basim Khajwal <basimkhajwal@gmail.com>
2015-02-08 12:02:50 +0000

    Added some test code

--- js/world/gameWorld.js
+++ js/world/gameWorld.js
@@ -23,12 +23,25 @@ Zamboni.World.GameWorld = {
         //All the private methods and variables
         var tiledMap = Engine.TiledMap.create(50, 30, 20, 20);
 
+        var level = Zamboni.World.LevelParser.parse(Engine.AssetManager.getAsset(Zamboni.Utils.GameSettings.levels.TEST));
+
+        (function () {
+            var i;
+
+            for (i = 0; i < level.length; i += 1) {
+                tiledMap.setTileAt(i / 50, i % 50, level[i]);
+            }
+        }());
+
+        tiledMap.putRenderable(1, Engine.AssetManager.getAsset(Zamboni.Utils.Assets.GRASS));
+        tiledMap.putRenderable(2, Engine.AssetManager.getAsset(Zamboni.Utils.Assets.GRASS_DARK));
+
         //Return all the public methods and variables
         return {
 
             //Render the world on the context ctx
             render: function (ctx) {
-
+                tiledMap.render(ctx);
             },
 
             //Update the world with time delta
