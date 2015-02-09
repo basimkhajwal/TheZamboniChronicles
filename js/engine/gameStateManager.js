f5e7f4ae810fd7f32cd99d828acab1a34ad7049f
Basim Khajwal <basimkhajwal@gmail.com>
2015-02-09 16:47:43 +0000

    Merged game state and game state manager to avoid naming issues

--- js/engine/gameStateManager.js
+++ js/engine/gameStateManager.js
@@ -2,6 +2,47 @@
 var Engine = Engine || {};
 
 /*
+*   Game state class to be extended but provides the basic function necessary for a state
+*   to be used by the GameStateManager
+*/
+Engine.GameState = {
+
+    /*
+    *   Create a new game state, has no base functionality but offers all the default functions
+    */
+    create: function () {
+
+        //Return a closure
+        return {
+            //Called when this game is added to the game, with the game passed as the parameter
+            onCreate: function (game) {
+
+            },
+
+            //Called around 60 times per second with the delta since the last update
+            update: function (delta) {
+
+            },
+
+            //Called around 60 times per second with the canvas context for rendering
+            render: function (context) {
+
+            },
+
+            //Called when the state is removed from the game and is replaced by another
+            onDestroy: function () {
+
+            }
+
+        };
+
+    }
+
+
+};
+
+
+/*
 *   Game state manager which holds the current state of the game and passes the rendering and update methods to it,
 *   provides an abstraction to remove code from the main game class
 */
