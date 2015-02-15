e0fca49eab4ed44695a58953204389530d5eb794
Zamboni13 <Chipigp8@gmail.com>
2015-02-15 21:22:16 +0000

    Maybe now

--- js/utils/assets.js
+++ js/utils/assets.js
@@ -13,8 +13,26 @@ Zamboni.Utils.Assets = {
     MENU_BG_FUZZY: "img/menu-bg-fuzzy.png",
 
     //The tiles
+    BLACK: "img/tiles/black.png",
+    BLACK_DARK: "img/tiles/black_dark.png",
+    CLOUDS: "img/tiles/clouds.png",
+    CLOUDS_DARK: "img/tiles/clouds_dark.png",
     GRASS: "img/tiles/grass.png",
     GRASS_DARK: "img/tiles/grass_dark.png",
+    GREY: "img/tiles/grey.png",
+    GREY_DARK: "img/tiles/grey_dark.png",
+    ORANGE: "img/tiles/orange.png",
+    ORANGE_DARK: "img/tiles/orange_dark.png",
+    PURPLE: "img/tiles/purple.png",
+    PURPLE_DARK: "img/tiles/purple_dark.png",
+    RED: "img/tiles/red.png",
+    RED_DARK: "img/tiles/red_dark.png",
+    SKY: "img/tiles/sky.png",
+    SKY_DARK: "img/tiles/sky_dark.png",
+    TURQUOISE: "img/tiles/turquoise.png",
+    TURQUOISE_DARK: "img/tiles/turquoise_dark.png",
+    YELLOW: "img/tiles/yellow.png",
+    YELLOW_DARK: "img/tiles/yellow_dark.png",
 
     //The sprites
     PLAYER: "img/sprites/player.png"
