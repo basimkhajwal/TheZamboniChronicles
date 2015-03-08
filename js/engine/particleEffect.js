f09e57420f3c8082ae699b7eca1ba8db64e0a4b6
Basim Khajwal <basimkhajwal@gmail.com>
2015-03-08 11:38:53 +0000

    Made base things for particle effects, no coding done as of yet

---
+++ js/engine/particleEffect.js
@@ -0,0 +1,12 @@
+//The engine module for use
+var Engine = Engine || {};
+
+
+
+/*
+*   A particle effect class for use in game, there are the following sections:
+*
+*   1. Particle Effect - A base object to hold details about each particle
+*   2. Manager - holds a group of particle effects and renders them
+*
+*/
