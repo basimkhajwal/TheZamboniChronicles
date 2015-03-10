ccd54f12dedc60bb2cc6e983a00a3b9c4e67eb7c
Basim Khajwal <basimkhajwal@gmail.com>
2015-03-10 19:05:28 +0000

    Made complete animation class

---
+++ js/engine/animation.js
@@ -0,0 +1,107 @@
+//Use the engine module
+var Engine = Engine || {};
+
+
+
+/*
+*   An animation class for holding a set of key frames and a time limit
+*   which will be used for sprites, tiles etc.
+*/
+Engine.Animation = {
+
+    /*
+    *   Create a new animation with the frames given and a time
+    *   per frame and whether to restart or not
+    */
+    create: function (frames, frameDuration, onLoop) {
+        "use strict";
+
+        //The total number of frames
+        var numFrames = frames.length,
+
+            //The index of the frame that we are at
+            frameIndex = 0,
+
+            //Whether or not this animation is finished
+            stopped = false,
+
+            //The current time (in seconds) of the animation that we are in
+            currentTime = 0;
+
+
+        //Return a closure with all the public fields
+        return {
+
+            // ------------------ Updating Method(s) -------------------
+
+            //Update the animation by the specified amount
+            update: function (delta) {
+
+                //If the animation is stopped then quit early
+                if (stopped) {
+                    return;
+                }
+
+                //Advance the time
+                currentTime += delta;
+
+                //If we have finished the current frame (repeat for large deltas if running slow)
+                while (currentTime > frameDuration) {
+
+                    //Set back the current time and advance the frame
+                    currentTime -= frameDuration;
+                    frameIndex += 1;
+
+                    //Check if frame has gone off
+                    if (frameIndex >= numFrames) {
+
+                        //If we are looping then reset
+                        if (onLoop) {
+                            frameIndex = 0;
+                        } else {
+                            //Otherwise animation has ended
+                            frameIndex = numFrames - 1;
+                            stopped = true;
+
+                            //Break function execution
+                            return;
+                        }
+
+                    }
+                }
+            },
+
+            // ------------------ Getters & Setters -------------------
+            getCurrentFrame: function () {
+                return frames[frameIndex];
+            },
+
+            setFrameIndex: function (newIndex) {
+                frameIndex = newIndex;
+            },
+
+            getFrameIndex: function () {
+                return frameIndex;
+            },
+
+            getCurrentTime: function () {
+                return currentTime;
+            },
+
+            setOnLoop: function (setLoop) {
+                onLoop = setLoop;
+            },
+
+            isOnLoop: function () {
+                return onLoop;
+            },
+
+            isFinished: function () {
+                return stopped;
+            }
+
+        };
+    }
+
+
+};
