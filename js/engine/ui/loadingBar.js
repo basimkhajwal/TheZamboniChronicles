de99d0d4663cc9859d28364ea1933233e4960efe
Basim Khajwal <basimkhajwal@gmail.com>
2015-03-14 19:44:43 +0000

    Made it pretty

--- js/engine/ui/loadingBar.js
+++ js/engine/ui/loadingBar.js
@@ -1,31 +1,47 @@
-//Use the Engine module and the sub UI module
+//Use the Engine module
 var Engine = Engine || {};
+
+//Use the UI submodule
 Engine.UI = Engine.UI || {};
 
-
+/*
+*   A loading bar utility class that holds a percentage of a bar
+*   to draw and has a render method to draw it correcly
+*/
 Engine.UI.LoadingBar = {
 
-
+    /*
+    *   Create a new loading bar with the given properties (not all are compulsory for initialisation)
+    */
     create: function (barY, barWidth, barHeight, canvasWidth, barColour) {
         "use strict";
 
+        //Set the variable default values if not already set
         barColour = barColour || "#000";
         canvasWidth = canvasWidth || 1000;
         barHeight = barHeight || 30;
         barWidth = barWidth || canvasWidth;
         barY = barY || 100;
 
+        //The percentage of the bar that is done (initially 0)
         var currentPercentage = 0,
+
+            //The position
             barX = (canvasWidth - barWidth) / 2;
 
+        //Return the public closure methdos
         return {
 
+            //Render the loading bar on the canvas context provided
             render: function (ctx) {
+                //Set the colour
                 ctx.fillStyle = barColour;
+
+                //Draw the rectangle
                 ctx.fillRect(barX, barY, barWidth * currentPercentage, barHeight);
             },
 
-            //The getters and setters
+            // ------------------ The Getters & Setters ----------------------------
 
             setPercentage: function (p) {
                 currentPercentage = p;
