3b0a0e50188501552361e7ac0626f8d8db9fa1f3
Basim Khajwal <basimkhajwal@gmail.com>
2015-03-23 17:35:14 +0000

    Begun writing a lava emitter (a bit copied atm)

--- js/world/gameWorld.js
+++ js/world/gameWorld.js
@@ -178,6 +178,38 @@ Zamboni.World.GameWorld = {
                 particlesPerSecond: 0
             }),
 
+            //A particle emitter for lava areas
+            lavaEmitter = Engine.ParticleEmitter.create({
+
+                //The default position (will be changed as rendered / updated)
+                x: 0,
+                y: 0,
+
+                //The gravity is high but no side accel
+                ax: 0,
+                ay: 30,
+
+                //An angle between 0 and 180
+                angle: 90,
+                angleVariance: 150,
+
+                //How fast different particles will go
+                speed: 40,
+                speedVariance: 10,
+
+                particleWidth: 6,
+                particleHeight: 6,
+
+                //A short life span (in seconds)
+                lifeSpan: 0.3,
+
+                startColour: Engine.Colour.create(160, 120, 0, 255),
+                endColour: Engine.Colour.create(110, 80, 0, 125),
+
+                maxParticles: 60,
+                particlesPerSecond: 0
+            }),
+
             //Manage the background stuff (clouds and parallax scrolling etc)
             backgroundManager = (function () {
 
