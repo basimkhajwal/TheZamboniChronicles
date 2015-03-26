1bb298ce3c0dffe6d3a634920a48f2997212f6bf
Basim Khajwal <basimkhajwal@gmail.com>
2015-03-26 17:35:07 +0000

    Fixed naming errors, now I need to add the functions and rename/fix stuff

--- js/world/levelParser.js
+++ js/world/levelParser.js
@@ -3,8 +3,6 @@ var Zamboni = Zamboni || {};
 var Engine = Engine || {};
 
 //The submodules
-Engine.GameEntity = Engine.GameEntity || {};
-
 Zamboni.World = Zamboni.World || {};
 
 
@@ -21,11 +19,13 @@ Zamboni.World.LevelParser = (function () {
     return {
 
         //The visible outside parse function
-        parseLevel: function (levelJSON) {
-
+        parseLevel: function (fileText, worldDescriptor) {
 
             //Parse the actual JSOn Object first
             var jsonObj = JSON.parse(fileText),
+
+                //Iterating variable
+                i,
 
                 //The tile layer from the JSON
                 tiles = jsonObj.layers[0].data,
@@ -37,30 +37,30 @@ Zamboni.World.LevelParser = (function () {
                 objects = jsonObj.layers[1].objects;
 
             //Create a new tiled map for the level
-            tiledMap = Engine.TiledMap.create(jsonObj.width, jsonObj.height, 20, 20);
+            worldDescriptor.tiledMap = Engine.TiledMap.create(jsonObj.width, jsonObj.height, 20, 20);
 
             //Put all the renderable tiles into the tiled maps renderable so that they are rendered correctly
 
             //Use a loop for less lines of code
             for (tile in Zamboni.Utils.GameSettings.tiles) {
                 if (Zamboni.Utils.GameSettings.tiles.hasOwnProperty(tile) && Zamboni.Utils.GameSettings.assets.hasOwnProperty(tile)) {
-                    tiledMap.putRenderable(Zamboni.Utils.GameSettings.tiles[tile], Engine.AssetManager.getAsset(Zamboni.Utils.GameSettings.assets[tile]));
+                    worldDescriptor.tiledMap.putRenderable(Zamboni.Utils.GameSettings.tiles[tile], Engine.AssetManager.getAsset(Zamboni.Utils.GameSettings.assets[tile]));
                 }
             }
 
             //Set the camera variables
-            worldWidth = tiledMap.getWidth() * tiledMap.getTileWidth();
-            worldHeight = tiledMap.getHeight() * tiledMap.getTileHeight();
+            worldDescriptor.worldWidth = worldDescriptor.tiledMap.getWidth() * worldDescriptor.tiledMap.getTileWidth();
+            worldDescriptor.worldHeight = worldDescriptor.tiledMap.getHeight() * worldDescriptor.tiledMap.getTileHeight();
 
             //Where the camera could end up at max to prevent dodgy camera movement off the tiled map
-            maxCameraX = worldWidth - 1000;
-            maxCameraY = worldHeight - 600;
+            worldDescriptor.maxCameraX = worldDescriptor.worldWidth - 1000;
+            worldDescriptor.maxCameraY = worldDescriptor.worldHeight - 600;
 
             //Set all the background tiles
             for (i = 0; i < tiles.length; i += 1) {
 
                 //Set the correct row and column to the value that is at that tile
-                tiledMap.setTileAt(Math.floor(i / jsonObj.width), i % jsonObj.width, tiles[i]);
+                worldDescriptor.tiledMap.setTileAt(Math.floor(i / jsonObj.width), i % jsonObj.width, tiles[i]);
             }
 
             //Loop over every object defined in the second layer and call the correct function to parse it
@@ -68,44 +68,35 @@ Zamboni.World.LevelParser = (function () {
 
                 switch (objects[i].type) {
 
-                    case "player":
-                        parsePlayer(objects[i]);
-                        break;
+                case "player":
+                    parsePlayer(objects[i]);
+                    break;
 
-                    case "lava":
-                        parseLava(objects[i]);
-                        break;
+                case "lava":
+                    parseLava(objects[i]);
+                    break;
 
-                    case "enemy":
-                        parseEnemy(objects[i]);
-                        break;
+                case "enemy":
+                    parseEnemy(objects[i]);
+                    break;
 
-                    case "platform":
-                        parsePlatform(objects[i]);
-                        break;
+                case "platform":
+                    parsePlatform(objects[i]);
+                    break;
 
-                    case "spikes":
-                        parseSpikes(objects[i]);
-                        break;
+                case "spikes":
+                    parseSpikes(objects[i]);
+                    break;
 
-                    case "ladder":
-                        parseLadder(objects[i]);
-                        break;
+                case "ladder":
+                    parseLadder(objects[i]);
+                    break;
                 }
             }
 
-            //Generate the background
-            backgroundManager.create();
-
             //Get the collision function
-            tiledCollision = tiledMap.isCellBlocked;
-            entityCollisions.push(tiledMap.generateCollisionFunction());
-
-            //Merge the collision functions all into one
-            entityCollision = mergeAllCollisions(entityCollisions);
-            ladderCollision = mergeAllCollisions(ladderCollisions);
-            enemyCollision = mergeAllCollisions(enemyCollisions);
-
+            worldDescriptor.tiledCollision = worldDescriptor.tiledMap.isCellBlocked;
+            worldDescriptor.entityCollisions.push(worldDescriptor.tiledMap.generateCollisionFunction());
 
         }
 
