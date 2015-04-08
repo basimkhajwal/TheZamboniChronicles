e37a1a31ff8576f788a2063ec7fc87451d5667d0
Basim Khajwal <basimkhajwal@gmail.com>
2015-04-08 23:20:42 +0100

    Added basic layout stuff

---
+++ js/states/levelState.js
@@ -0,0 +1,23 @@
+//Use the following modules
+var Engine = Engine || {};
+var Zamboni = Zamboni || {};
+
+//Sub-modules
+Zamboni.States = Zamboni.States || {};
+Zamboni.Utils = Zamboni.Utils || {};
+Engine.UI = Engine.UI || {};
+
+/*
+*   A state which allows the user to select from a choice of levels
+*   and then creates the appropriate game state
+*/
+Zamboni.States.LevelState = {
+
+    create: function () {
+        "use strict";
+
+
+
+    }
+
+};
