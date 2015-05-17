10dbf7465564828c4545896ee5c0fc7ac8e94a13
Basim Khajwal <basimkhajwal@gmail.com>
2015-05-17 19:36:59 +0100

    Added basic gui code

--- js/states/gameState.js
+++ js/states/gameState.js
@@ -15,7 +15,16 @@ Zamboni.States.GameState = {
 
         var state = Engine.GameState.create(),
             game = null,
-            world = Zamboni.World.GameWorld.create();
+            world = Zamboni.World.GameWorld.create(),
+
+            //GUI
+            coinText = Engine.UI.TextArea.create(100, 10, ""),
+            coinImg = Engine.AssetManager.getAsset(Zamboni.Utils.GameSettings.assets.COIN);
+
+        coinText.setFamily(Zamboni.Utils.GameSettings.gameFont);
+        coinText.setBaseline("top");
+        coinText.setSize(30);
+        coinText.setColour(Zamboni.Utils.ColourScheme.ORANGE);
 
 
         state.onCreate = function (g) {
@@ -28,10 +37,16 @@ Zamboni.States.GameState = {
             //Draw the HUD bar
             ctx.fillStyle = "rgb(0,0,0,0.3)";
             ctx.fillRect(0, 0, 1000, 50);
+
+            //Draw the number of coins
+            coinText.render(ctx);
         };
 
         state.update = function (delta) {
             world.update(delta);
+
+            //Update GUI values
+            coinText.setText(world.playerDescriptor.coinsCollected);
         };
 
         return state;
