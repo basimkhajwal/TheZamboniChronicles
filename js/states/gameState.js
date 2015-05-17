34b595628dd82699ca42890b25206d8596b4753a
Basim Khajwal <basimkhajwal@gmail.com>
2015-05-17 20:24:53 +0100

    Draws a coin on the left

--- js/states/gameState.js
+++ js/states/gameState.js
@@ -18,14 +18,13 @@ Zamboni.States.GameState = {
             world = Zamboni.World.GameWorld.create(),
 
             //GUI
-            coinText = Engine.UI.TextArea.create(100, 10, ""),
-            coinImg = Engine.AssetManager.getAsset(Zamboni.Utils.GameSettings.assets.COIN);
+            coinText = Engine.UI.TextArea.create(35, 13, ""),
+            coinImg = Engine.AssetManager.getAsset(Zamboni.Utils.GameSettings.assets.COIN_1);
 
         coinText.setFamily(Zamboni.Utils.GameSettings.gameFont);
         coinText.setBaseline("top");
-        coinText.setSize(30);
+        coinText.setSize(20);
         coinText.setColour(Zamboni.Utils.ColourScheme.ORANGE);
-
 
         state.onCreate = function (g) {
             game = g;
@@ -38,7 +37,8 @@ Zamboni.States.GameState = {
             ctx.fillStyle = "rgb(0,0,0,0.3)";
             ctx.fillRect(0, 0, 1000, 50);
 
-            //Draw the number of coins
+            //Draw the number of coins and an image
+            ctx.drawImage(coinImg, 5, 15, 20, 20);
             coinText.render(ctx);
         };
 
