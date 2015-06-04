ff3be859e4d7cebd7cce565b83c2bdb28f2b0b8b
basimkhajwal <basimkhajwal@gmail.com>
2015-06-04 15:26:36 +0100

    Setup title

--- js/states/levelState.js
+++ js/states/levelState.js
@@ -15,20 +15,41 @@ Zamboni.States.LevelState = {
 
     create: function (gameDescriptor) {
         "use strict";
-
+        
         //Create an empty state object
         var state = Engine.GameState.create(),
 
             //The global game object
-            game;
-
+            game,
+            
+            //The top title
+            titleText = Engine.UI.TextArea.create(500, 10, "Level Select");
+            
+        
+        //Setup all the title settings
+        titleText.setFamily(Zamboni.Utils.GameSettings.gameFont);
+        titleText.setBaseline("top");
+        titleText.setSize(30);
+        titleText.setColour(Zamboni.Utils.ColourScheme.POMEGRANATE);
+        
+        //Setup all the stuff for drawing all the levels
+        (function () {
+            
+            
+            
+        }());
+      
         //When the state is created with a new game
         state.onCreate = function (g) {
             game = g;
         };
 
         state.render = function (ctx) {
-
+            
+            //Clear the screen
+            ctx.fillStyle = Engine.Utils.ColourScheme.PETER_RIVER;
+            ctx.fillRect(0, 0, 1000, 600);
+            
         };
 
         state.update = function (delta) {
