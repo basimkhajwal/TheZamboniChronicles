5b6d59cb13631c5e0f326a7edbc9a71afe42f9e7
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-14 19:36:19 +0100

    Creaed new player manager

---
+++ js/world/playerManager.js
@@ -0,0 +1,26 @@
+//Modules needed
+var Zamboni = Zamboni || {};
+var Engine = Engine || {};
+
+/*
+*   Player manager to move code from the world updater
+*/
+Zamboni.World.PlayerManager = {
+
+    /*
+    *   Create a new player manager with the specified worldDescriptor and the playerDescriptor
+    */
+    create: function (worldDescriptor, playerDescriptor) {
+        "use strict";
+
+
+
+        //Return a closure with the public methods
+        return {
+
+        };
+
+    }
+
+
+};
