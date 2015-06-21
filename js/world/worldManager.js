091342bc1b4934e0093a3182e787fe8c1c6ea2c6
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-21 17:26:03 +0100

    Added boilerplate

---
+++ js/world/worldManager.js
@@ -0,0 +1,23 @@
+var Zamboni = Zamboni || {};
+var Engine = Engine || {};
+
+/*
+*   Game manager to handle the game logic in the world
+*/
+Zamboni.World.GameManager = {
+
+    create: function (worldDescriptor) {
+
+        "use strict";
+
+        //The closure with the public methods
+        return {
+
+            update: function (delta) {
+
+            }
+
+        };
+    }
+
+};
---
+++ js/world/worldRenderer.js
@@ -0,0 +1,25 @@
+var Zamboni = Zamboni || {};
+var Engine = Engine || {};
+
+/*
+*   Game renderer to render the game objects
+*/
+Zamboni.World.GameManager = {
+
+    create: function (worldDescriptor) {
+
+        "use strict";
+
+
+
+        //The closure with the public methods
+        return {
+
+            render: function (ctx) {
+
+            }
+
+        };
+    }
+
+};
